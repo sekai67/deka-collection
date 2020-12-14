e5bd905e4ec0e326bb5be5d6164fdf456270cbdd
Kazuki Sawada <kazuki@6715.jp>
2020-12-14 23:45:02 +0900

    dispatch in root

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,9 +1,12 @@
 import { Provider } from "react-redux";
 import store from "./stores";
+import { fetchAccounts } from "./stores/accounts";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./views/App";
 import GlobalStyle from "./styles/global";
+
+store.dispatch(fetchAccounts());
 
 ReactDOM.render(
 	<React.StrictMode>
