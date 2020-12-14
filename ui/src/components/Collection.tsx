5e368e23c842142668add939b0af3d29b4ab45c3
Kazuki Sawada <kazuki@6715.jp>
2020-12-14 20:35:53 +0900

    use emotion on Collection

--- ui/src/components/Collection.tsx
+++ ui/src/components/Collection.tsx
@@ -1,17 +1,22 @@
-import "../styles/Collection.scoped.scss";
-
+import styled from "@emotion/styled";
 import { useSelector } from "react-redux";
 import Profile from "./Profile";
+import Spinner from "./Spinner";
+
+const Container = styled.div({
+	display: "flex",
+	flexWrap: "wrap",
+});
 
 export default function Component() {
 	const accounts = useSelector(state => state.accounts.value);
 
 	return (
-		<section>
-			{!accounts.length && <div className="loading dot-windmill" />}
+		<Container>
+			{!accounts.length && <Spinner />}
 			{accounts.map(account => (
 				<Profile key={account.screen_name} account={account} />
 			))}
-		</section>
+		</Container>
 	);
 }
