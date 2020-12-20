9d02bd70ed05e0e390e806af05331d833ef55766
Kazuki Sawada <kazuki@6715.jp>
2020-12-21 08:41:18 +0900

    move replies

--- ui/src/replies/index.ts
+++
@@ -1,7 +0,0 @@
-const repliesCount = 70;
-
-export default Array.from(new Array(repliesCount).keys()).map(num => {
-	const numKey = `000${num + 1}`.substr(-3);
-	const reply: string = require(`!!raw-loader!./reply${numKey}.txt`).default;
-	return reply;
-});
