92f4a49e6eca864b4faca7f50d2403d590e5eecd
Kazuki Sawada <kazuki@6715.jp>
2020-12-21 05:50:48 +0900

    update twitter profile fetching method

--- api/src/fetch.ts
+++ api/src/fetch.ts
@@ -1,21 +1,46 @@
-import { load } from "cheerio";
 import { Account } from "./kv";
 
-export const fetchAccount = async (screenName: string): Promise<Account> => {
-	const resp = await fetch(`https://mobile.twitter.com/${screenName}`);
+type GuestActivateResponse = {
+	guest_token: string;
+};
+type UsersShowResponse = {
+	id_str: string;
+	name: string;
+	screen_name: string;
+	description: string;
+};
+
+const headers = {
+	Authorization:
+		"Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+};
+
+export const fetchAccount = async (screen_name: string): Promise<Account> => {
+	const tokenResp = await fetch("https://api.twitter.com/1.1/guest/activate.json", {
+		method: "POST",
+		headers,
+	});
+	if (!tokenResp.ok) {
+		throw new Error(`Failed to issue guest token, status=${tokenResp.status}`);
+	}
+	const { guest_token }: GuestActivateResponse = await tokenResp.json();
+
+	const resp = await fetch(`https://api.twitter.com/1.1/users/show.json?screen_name=${screen_name}`, {
+		headers: Object.assign(
+			{
+				"X-Guest-Token": guest_token,
+			},
+			headers,
+		),
+	});
 	if (!resp.ok) {
-		throw new Error(`Failed to fetch profile: status=${resp.status}`);
+		throw new Error(`Failed to get user info, status=${tokenResp.status}`);
 	}
 
-	const $ = load(await resp.text());
-	const account = {
-		screen_name: $(".screen-name").text().trim(),
-		full_name: $(".profile .fullname").text().trim(),
-		bio: $(".bio").text().trim(),
+	const user: UsersShowResponse = await resp.json();
+	return {
+		screen_name: user.screen_name,
+		full_name: user.name,
+		bio: user.description,
 	};
-
-	if (!account.screen_name) {
-		throw new Error(`Failed to parse profile`);
-	}
-	return account;
 };
